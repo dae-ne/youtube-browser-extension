d2378295fc5498805ccd15c46133de8e1dfd7037
dae-ne <rafalcz29@gmail.com>
2024-03-04 01:44:58 +0100

    Fix errors

--- background.js
+++ background.js
@@ -3,6 +3,10 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     return;
   }
 
+  if (tab.url.includes('youtube.com') && !tab.url.includes('watch')) {
+    chrome.tabs.sendMessage(tabId, { action: 'disconnect-ads-observer' });
+  }
+
   if (tab.url.includes('youtube.com/shorts')) {
     chrome.tabs.sendMessage(tabId, { action: 'show-shorts-to-video-button' });
     return;
@@ -11,7 +15,5 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (tab.url.includes('youtube.com/watch')) {
     chrome.tabs.sendMessage(tabId, { action: 'skip-ads' });
     return;
-  } else {
-    chrome.tabs.sendMessage(tabId, { action: 'disconnect-ads-observer' });
   }
 });
