0978ebdc9e1c97db3bf9e4960083edd654a06eac
dae-ne <rafalcz29@gmail.com>
2024-03-08 09:16:53 +0100

    Add shorts css file

--- shorts.js
+++ shorts.js
@@ -16,7 +16,7 @@
       return;
     }
 
-    let button = actions.querySelector('#yt-shorts-to-video-btn');
+    let button = actions.querySelector('.ytext-shorts-video-btn');
 
     if (button) {
       return;
@@ -24,18 +24,18 @@
 
     const image = document.createElement('img');
     image.src = chrome.runtime.getURL('video-play.svg');
-    image.style.width = '24px';
-    image.style.filter = 'invert(1)';
+    image.classList.add('ytext-shorts-video-btn-icon');
 
     button = document.createElement('button');
-    button.id = 'yt-shorts-to-video-btn';
+
     button.classList.add(
+      'ytext-shorts-video-btn',
       'yt-spec-button-shape-next',
       'yt-spec-button-shape-next--tonal',
       'yt-spec-button-shape-next--mono',
       'yt-spec-button-shape-next--size-l',
       'yt-spec-button-shape-next--icon-button');
-    button.style.marginTop = '16px';
+
     button.appendChild(image);
     actions.insertBefore(button, actions.querySelector('#menu-button'));
 
