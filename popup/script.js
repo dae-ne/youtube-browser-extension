d7566839fa4caffccb3dee3de661ab6f274accec
dae-ne <rafalcz29@gmail.com>
2024-03-10 20:00:38 +0100

    Update popup and options

--- popup/script.js
+++ popup/script.js
@@ -1,5 +1,17 @@
 const optionsButton = document.querySelector('.options-btn');
+const githubButton = document.querySelector('.github-btn');
+const issuesButton = document.querySelector('.issues-btn');
 
 optionsButton.addEventListener('click', () => {
   chrome.runtime.openOptionsPage();
+});
+
+githubButton.addEventListener('click', () => {
+  const githubUrl = 'https://github.com/dae-ne/yt-extension';
+  window.open(githubUrl, '_blank');
+});
+
+issuesButton.addEventListener('click', () => {
+  const issuesUrl = 'https://github.com/dae-ne/yt-extension/issues';
+  window.open(issuesUrl, '_blank');
 });
