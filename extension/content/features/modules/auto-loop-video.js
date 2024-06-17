0f7812bdb56872f7aaa9715013b8a1c8210775a6
dae-ne <rafalcz29@gmail.com>
2024-06-17 19:27:42 +0200

    Add retries to the looping video feature

--- extension/content/features/modules/auto-loop-video.js
+++ extension/content/features/modules/auto-loop-video.js
@@ -1,4 +1,13 @@
 /**
+ * The interval in milliseconds for recurring tasks.
+ *
+ * @constant
+ * @type {number}
+ * @default
+ */
+const INTERVAL_MS = 200;
+
+/**
  * Finds the video element and sets the loop property.
  *
  * @param {boolean} loop - The value to set the loop property to.
@@ -7,6 +16,7 @@ export function loopVideo(loop = true) {
   const video = document.querySelector('video');
 
   if (!video) {
+    setTimeout(() => loopVideo(loop), INTERVAL_MS);
     return;
   }
 
