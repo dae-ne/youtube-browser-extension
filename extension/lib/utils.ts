e7bbf655eb2280a336a1d4c85f0c49605ee9febe
dae-ne <rafalcz29@gmail.com>
2025-02-21 22:43:01 +0100

    Remove unused utils functions

--- extension/lib/utils.ts
+++ extension/lib/utils.ts
@@ -1,39 +1,4 @@
 /**
- * The CSS class prefix used by the extension.
- */
-const EXTENSION_CSS_CLASS_PREFIX = 'yte-';
-
-/**
- * Checks if a video is opened on the current page.
- *
- * @remarks
- * A miniplayer is also considered a video page, so this function returns true if a video is
- * opened in the miniplayer (e.g. on the YouTube homepage).
- *
- * @returns Whether the current page is a video page.
- */
-export function isVideoOpened(): boolean {
-    const isWatchPage = window.location.href.includes('youtube.com/watch');
-    return isWatchPage || isMiniplayerOpened();
-}
-
-/**
- * Checks if a miniplayer is opened on the current page.
- *
- * @returns Whether the current page has a miniplayer opened.
- */
-export function isMiniplayerOpened(): boolean {
-    const miniplayer = document.querySelector('.miniplayer');
-    const container = miniplayer?.querySelector('#player-container');
-
-    if (!container) {
-        return false;
-    }
-
-    return container.childNodes.length > 0;
-}
-
-/**
  * Checks if the current page is a shorts page.
  *
  * @returns Whether the current page is a shorts page.
@@ -53,36 +18,6 @@ export function removeCssClass(className: string): void {
 }
 
 /**
- * Removes all elements with a specific class prefix from the page.
- *
- * @remarks
- * This function is useful for cleaning up the page after the extension is disabled or to remove
- * global css after opening a new page. By default, it removes only the classes that start with
- * 'yte-', so only the classes added by this extension.
- *
- * @param classNamePrefix - The class name prefix used to filter the elements.
- */
-export function removeCssClassesByClassNamePrefix(
-    classNamePrefix = EXTENSION_CSS_CLASS_PREFIX
-): void {
-    const isExtensionClassName = classNamePrefix.startsWith(EXTENSION_CSS_CLASS_PREFIX);
-
-    if (!isExtensionClassName) {
-        return;
-    }
-
-    const elements = document.querySelectorAll(`[class*="${classNamePrefix}"]`);
-
-    elements.forEach(element => {
-        const classNamesToRemove = Array.from(element.classList).filter(className =>
-            className.startsWith(classNamePrefix)
-        );
-
-        element.classList.remove(...classNamesToRemove);
-    });
-}
-
-/**
  * Adds a CSS class to the body element.
  *
  * @param className - The name of the CSS class to add.
@@ -97,20 +32,4 @@ export function addCssClassToBody(className: string): boolean {
 
     body.classList.add(className);
     return true;
-}
-
-/**
- * Gets the video element on the current page. The miniplayer is prioritized over the main player.
- *
- * @remarks
- * It returns only the video from the watch page or the miniplayer. It does not return the video
- * from the shorts page.
- *
- * @returns The video element on the current page.
- */
-export function getMainVideoElement(): HTMLVideoElement | null {
-    return (
-        document.querySelector('.miniplayer video') ??
-        document.querySelector('ytd-player:not(.ytd-shorts) video')
-    );
 }
